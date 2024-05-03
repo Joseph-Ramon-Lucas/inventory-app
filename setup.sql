36cd7ea8a9056c6ad072d33cd8a171e6e3a17ad3
Joseph-Ramon-Lucas <joseph-ramon-lucas@gmail.com>
2024-05-03 10:56:34 -0400

    created junction table for users and their stuff

--- setup.sql
+++ setup.sql
@@ -5,7 +5,7 @@ CREATE DATABASE InventoryDb;
 \c inventorydb;
 
 CREATE TABLE users (
-    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    userId INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     username TEXT NOT NULL,
     password TEXT
 );
@@ -13,8 +13,8 @@ CREATE TABLE users (
 
 
 CREATE TABLE stuff (
-    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    itemName TEXT NOT NULL,
+    itemId INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    itemName TEXT NOT NULL
     -- quantity FLOAT,
     -- itemType TEXT,
     -- itemValue FLOAT,
@@ -22,12 +22,23 @@ CREATE TABLE stuff (
     -- FOREIGN KEY (owner) REFERENCES owners(ownerId)
 );
 
-CREATE TABLE owners(
-    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    ownerId INT REFERENCES users(id),
-    itemId INT,
-    FOREIGN KEY (itemId) REFERENCES stuff(id) 
+CREATE TABLE usersOwnStuff(
+    ownerId INT NOT NULL,
+    itemId INT NOT NULL,
+    CONSTRAINT PK_userOwnsStuff PRIMARY KEY (
+        ownerId,
+        itemId
+    ),
+    FOREIGN KEY (ownerId) REFERENCES users(userId),
+    FOREIGN KEY (itemId) REFERENCES stuff(itemId)
 );
 
 insert into users (username, password) values('joe', '1234');
+insert into stuff (itemName) values('phone');
+insert into stuff (itemName) values('watch');
+insert into usersOwnStuff(ownerId, itemId) values(1, 1);
+insert into usersOwnStuff(ownerId, itemId) values(1, 2);
+
+-- select username,itemname from usersOwnStuff join users on users.userId = ownerid join stuff on stuff.itemId=usersOwnStuff.itemId;
+
 
