85cf79fcc508cc2e3639c2ac456033d2d6e2778c
Joseph-Ramon-Lucas <joseph-ramon-lucas@gmail.com>
2024-05-05 17:02:23 -0400

    better config changes that fix ts compiling

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,17 +1,15 @@
-import express from 'express';
-import dotenv from 'dotenv'
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
 
-dotenv.config({path: '../.env'});
+dotenv.config({ path: "../.env" });
 
-const app = express();
-const port = process.env.PORT;
-// const port = 3000
+const app: Express = express();
+const port = process.env.PORT || 3000;
 
-
-app.get('/', (req, res) => {
-  res.send('Express + TypeScript Server');
+app.get("/", (req: Request, res: Response) => {
+    res.send("Express + TypeScript Server");
 });
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+    console.log(`[server]: Server is running at http://localhost:${port}`);
 });
