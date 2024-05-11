7c087d142db3c8a2f9e92de904fd2e4fab86d759
Joseph-Ramon-Lucas <76121642+Joseph-Ramon-Lucas@users.noreply.github.com>
2024-05-10 23:42:44 -0400

    added new route

--- back/src/server.ts
+++ back/src/server.ts
@@ -10,6 +10,12 @@ app.get("/", (req: Request, res: Response) => {
     res.send("Express + TypeScript Server");
 });
 
+app.get("/search", (req:Request, res:Response) => {
+    res.status(200);
+    
+})
+
+
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
