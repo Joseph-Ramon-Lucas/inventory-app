25bbb94e1e92a6e1319efca7e5b25acea64d372a
Joseph Lucas <joseph.ramon.lucas@gmail.com>
2024-05-12 01:11:21 -0400

    Implemented json body parsing

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,4 +1,4 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, Request, Response, json } from "express";
 import dotenv from "dotenv";
 
 dotenv.config({ path: "../.env" });
@@ -6,14 +6,38 @@ dotenv.config({ path: "../.env" });
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+//make every route public
+app.use(express.static("public"));
+
+//parse req.body in json
+app.use(json());
+
+
 app.get("/", (req: Request, res: Response) => {
-    res.send("Express + TypeScript Server");
+    res.send("Welcome to the Inventory App");
 });
 
-app.get("/search", (req:Request, res:Response) => {
-    res.status(200);
+//user authentication
+app.post("/register", (req: Request, res:Response) => {
+    let {username, password} = req.body;
+
+    console.log('reqbody:', req.body);
+
+    console.log(username, password, "omg!");
     
+    res.status(201).send(username+" registered!");
 })
+
+
+app.get("/search", (req:Request, res:Response) => {
+    let q = JSON.stringify(req.query.something);
+
+    console.log(q)
+    
+    res.status(200);
+    res.send(`searching for ${q}`)
+    
+});
 
 
 app.listen(port, () => {
