660dc4886494d9d5bd66378499805451c3d41972
Joseph-Ramon-Lucas <joseph-ramon-lucas@gmail.com>
2024-05-14 13:25:12 -0400

    getting started with drizzle

---
+++ back/src/db/index.ts
@@ -0,0 +1,12 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import dbEnv from "../../env.json";
+import * as schema from "./schema";
+
+const client = new Pool(dbEnv);
+
+export const db = drizzle(client, {
+    schema,
+});
+
+export const poop = "fart";
--- back/src/server.ts
+++ back/src/server.ts
@@ -1,12 +1,8 @@
 import express, { Express, Request, Response, json } from "express";
 import bcrypt from "bcrypt";
-
-import {Pool} from "pg";
-import dbEnv from "../env.json";
-
-// let dbEnv = dotenv.config({ path: "../env.json" });
-let db = new Pool(dbEnv)
-
+import { db } from "./db";
+import { eq } from "drizzle-orm";
+import { usersTable } from "./db/schema";
 
 // salts for password
 const saltRounds = 10;
@@ -20,15 +16,14 @@ app.use(express.static("public"));
 //parse req.body in json
 app.use(json());
 
-
 app.get("/", (req: Request, res: Response) => {
     res.send("Welcome to the Inventory App");
 });
 
 //user authentication
-app.post("/register", (req: Request, res:Response) => {
-    let {username, password} = req.body;
-    console.log('reqbody:', req.body);
+app.post("/register", (req: Request, res: Response) => {
+    let { username, password } = req.body;
+    console.log("reqbody:", req.body);
 
     const salt = bcrypt.genSaltSync(saltRounds);
     const hashWord = bcrypt.hashSync(password, saltRounds);
@@ -37,62 +32,54 @@ app.post("/register", (req: Request, res:Response) => {
 
     // check if this user already exists
     try {
-        db.query(
-            `SELECT username FROM users WHERE username = $1`, [username]
-        )
-        .then((result) => {
-            let dbResult = result.rows;
-
-            // can't use an existing username
-            if (dbResult.includes(username)) {
-                res.status(400).send(`Username ${username} is already in use`)
-            }
-        })
-        .catch((err) => {
-            res.status(500).send(err);
-            return
-        })
-    }
-    catch(e:any){
+        db.select()
+            .from(usersTable)
+            .where(eq(usersTable.username, username))
+            .limit(1)
+            .then((results) => {
+                if (results.length > 0) {
+                    res.status(400).send(
+                        `Username ${username} is already in use`
+                    );
+                }
+            })
+            .catch((err) => {
+                res.status(500).send(err);
+                return;
+            });
+    } catch (e: any) {
         console.error(e);
-        res.status(500)
-        return
+        res.status(500);
+        return;
     }
-
 
     // store new user
-    db.query(
-        `INSERT INTO users(username, password) VALUES($1, $2)`, [username, hashWord]
-    )
-    .then((result) => {
-        let dbResult = JSON.stringify(result.rows)
-        console.log(dbResult);
-        res.status(200).send(dbResult)
-    })
-    .catch((error) => {
-        console.error(JSON.stringify(error));
-        res.status(500).json({error: error});
-        return
-    })
+    db.query(`INSERT INTO users(username, password) VALUES($1, $2)`, [
+        username,
+        hashWord,
+    ])
+        .then((result) => {
+            let dbResult = JSON.stringify(result.rows);
+            console.log(dbResult);
+            res.status(200).send(dbResult);
+        })
+        .catch((error) => {
+            console.error(JSON.stringify(error));
+            res.status(500).json({ error: error });
+            return;
+        });
 
-
-
-
-    
     // res.status(201).send(username+" registered!");
-})
-
-
-app.get("/search", (req:Request, res:Response) => {
-    let q = JSON.stringify(req.query.something);
-
-    console.log(q)
-    
-    res.status(200);
-    res.send(`searching for ${q}`)
-    
 });
 
+app.get("/search", (req: Request, res: Response) => {
+    let q = JSON.stringify(req.query.something);
+
+    console.log(q);
+
+    res.status(200);
+    res.send(`searching for ${q}`);
+});
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
