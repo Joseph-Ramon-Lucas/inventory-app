2ce2c0e19decc755fe42573ca345b4222148d9cf
Joseph-Ramon-Lucas <joseph-ramon-lucas@gmail.com>
2024-05-31 13:09:03 -0400

    Fixed drizzle schema

--- back/src/db/schema.ts
+++ back/src/db/schema.ts
@@ -2,7 +2,7 @@ import { serial, text } from "drizzle-orm/pg-core";
 import { pgTable } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
-	userId: serial("userId").primaryKey(),
+	userId: serial("userid").primaryKey(),
 	username: text("username").notNull(),
 	password: text("password"),
 });
