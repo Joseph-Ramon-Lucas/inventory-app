3154f8be03f32aaca7f2e083fe2bd5c39faffa5e
Joseph Lucas <joseph.ramon.lucas@gmail.com>
2024-06-05 13:35:52 -0400

    Creating labels for inputs

--- front/src/components/CreateAccount.tsx
+++ front/src/components/CreateAccount.tsx
@@ -1,18 +1,34 @@
-import { useState } from "react";
-import "../App.css";
+import { useEffect, useState } from "react";
+// import "../App.css";
 import { Button } from "@/components/ui/button";
 import { Input } from "./ui/input";
+import { Label } from "./ui/label";
 
 
 export function CreateAccount() {
 
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+
+    useEffect(() => {
+        console.log(username)
+
+    }, [username]);
     return (
         <div>
             <h1>Create your Cabinet Account</h1>
             
-            <Input></Input>
+            <Label htmlFor="email">
+Email
+            </Label>
+            <Input type="text"
+                    id="username"
+                    placeholder="username"
+                    onChange={(event => {
+                        setUsername(event.target.value)
+                    })}
+                    
+            ></Input>
         </div>
     )
 }
