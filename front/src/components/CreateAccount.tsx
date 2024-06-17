5ba7cfbc859e242305c9c31db7018b17ca8a5437
Joseph-Ramon-Lucas <joseph-ramon-lucas@gmail.com>
2024-06-17 12:37:44 -0400

    Create account inputs are set

--- front/src/components/CreateAccount.tsx
+++ front/src/components/CreateAccount.tsx
@@ -4,15 +4,28 @@ import { Button } from "@/components/ui/button";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 export function CreateAccount() {
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
 	const [retypePassword, setRetypePassword] = useState("");
+	const [errorText, setErrorText] = useState("");
+	const [matching, setMatching] = useState(false);
 
 	useEffect(() => {
-		console.log(username);
-	}, [username]);
+		console.log(password, retypePassword);
+		if (password !== retypePassword) {
+			setErrorText("Passwords do not match!!");
+			setMatching(false);
+		} else {
+			setErrorText("");
+			setMatching(true);
+		}
+	}, [password, retypePassword]);
+
+	function handleSubmit(username: string, password: string) {}
+
 	return (
 		<div>
 			<Link to="/">
@@ -26,36 +39,45 @@ export function CreateAccount() {
 					<Label htmlFor="retypePassword">Retype Password</Label>
 				</div>
 				<div className="inputs">
-					<Input
-						type="username"
-						id="username"
-						placeholder="username"
-						onChange={(event) => {
-							setUsername(event.target.value);
-						}}
-					/>
+					<div>
+						<Input
+							type="username"
+							id="username"
+							placeholder="username"
+							onChange={(event) => {
+								setUsername(event.target.value);
+							}}
+						/>
+					</div>
+
 					<br />
-					<Input
-						type="password"
-						id="password"
-						placeholder="password"
-						onChange={(event) => {
-							setPassword(event.target.value);
-						}}
-					/>
+					<div>
+						<Input
+							type="password"
+							id="password"
+							placeholder="password"
+							onChange={(event) => {
+								setPassword(event.target.value);
+							}}
+						/>
+					</div>
+
 					<br />
-					<Input
-						type="password"
-						id="retypePassword"
-						placeholder="Retype your password"
-						onChange={(event) => {
-							setRetypePassword(event.target.value);
-						}}
-					/>
+					<div>
+						<Input
+							type="password"
+							id="retypePassword"
+							placeholder="Retype your password"
+							onChange={(event) => {
+								setRetypePassword(event.target.value);
+							}}
+						/>
+					</div>
 				</div>
 			</div>
+			<div id="errorDiv">{errorText ?? (errorText && true)}</div>
 			<div id="submit">
-				<Button>Submit</Button>
+				<Button onClick={() => handleSubmit(username, password)}>Submit</Button>
 			</div>
 		</div>
 	);
